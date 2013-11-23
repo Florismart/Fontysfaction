f0e38fc0154d5a6cda5d7163365e916099aa9eb5
Luca Fattorini <fattorini.luca@gmail.com>
2013-11-23 11:50:26 +0100

    fixed bug with typoEnum

--- src/com/fattorini/luca/android/fontysfaction/config/FontyTypo.java
+++ src/com/fattorini/luca/android/fontysfaction/config/FontyTypo.java
@@ -9,7 +9,7 @@ public enum FontyTypo {
 	regular, bold, light, regular_italic, bold_italic, light_italic;
 
 	public static FontyTypo fromIndex(final int index) {
-		if (index >= FontyTypo.values().length - 1 || index < 0)
+		if (index >= FontyTypo.values().length || index < 0)
 			return regular;
 		else
 			return FontyTypo.values()[index];
